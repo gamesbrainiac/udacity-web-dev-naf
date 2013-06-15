bae34cb6922ad79a59f4685dfa3c754734f4efe2
Nafiul Islam <gamesbrainiac@gmail.com>
2013-06-15 11:11:16 +0600

    Committing main file.

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+
+form = """
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<form action="/testform" method="POST">
+    <input name="q" />
+    <input type="submit"/>
+</form>
+</body>
+</html>
+"""
+
+# In method, it does not matter if you write it in upper or
+# lower case, both 'post' and 'POST' will work.
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        # This line below sets content to text/plain
+        # Meaning that it will print out nothing but
+        # plain text, so no HTML is rendered
+        # self.response.headers['Content-Type'] = 'text/plain'   # Uncomment to see effects of above comments
+        self.response.write(form)
+
+
+class TestHandler(webapp2.RedirectHandler):
+    # This defines what happens when a GET request is sent to the server
+    def get(self):
+        q = self.request.get("q")
+        self.response.write(q)
+
+    def post(self):
+        self.response.headers['Content-Type'] = 'text/plain'  # Sets content to text/plain
+        self.response.out.write(self.request)                 # Prints out the entire request
+        # q = self.request.get("q")
+        # self.response.write(q)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/testform', TestHandler),
 ], debug=True)

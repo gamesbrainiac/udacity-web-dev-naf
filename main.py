ed3fb754139893e2423a09f71a3c2cf7cd179b3c
Nafiul Islam <gamesbrainiac@gmail.com>
2013-06-21 01:44:08 +0600

    Finished problem set 2 with templates

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import webapp2
 import jinja2
 
-import ProblemSet2
+from google.appengine.ext import db
 
 
 # Setting up directory helper functions
@@ -15,92 +15,53 @@ JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR),
                                autoescape=True)
 
 
+class Art(db.Model):
+    title = db.StringProperty(required=True)
+    art = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
+
 class GenericHandler(webapp2.RequestHandler):
 
     def write(self, *args, **kwargs):
         return self.response.out.write(*args, **kwargs)
 
-    def render_template(self, template, context_data=None):
+    def render_template(self, template, context_data=None, **kwargs):
         if not context_data:
             context_data = {}
         renderer = JINJA_ENV.get_template(template)
-        return self.write(renderer.render(context_data))
+        return self.write(renderer.render(context_data, **kwargs))
 
 
 class HomePageHandler(GenericHandler):
 
-    def get(self):
-        return self.render_template('home_page.html')
+    def render_page(self, context_data=None, **kwargs):
+        art_works = db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
 
-
-class ThanksHandler(GenericHandler):
-
-    def get(self):
-        username = str(self.request.get('username'))
-        return self.render_template('thanks.html', context_data={
-            'username': username,
-        })
-
-
-class Rot13(GenericHandler):
+        return self.render_template('home_page.html', context_data=context_data, listOfArt=art_works, **kwargs)
 
     def get(self):
-        return self.render_template('rot-13.html', {})
+        return self.render_page()
 
     def post(self):
-        text = str(self.request.get('text'))
-        return_context = {
-            'text': ProblemSet2.rotate_thirteen(text),
-        }
-        return self.render_template('rot-13.html', return_context)
+        title = str(self.request.get('title'))
+        art = str(self.request.get('art'))
 
+        if title and art:
+            a = Art(title=title, art=art)
+            a.put()
 
-class SignupHandler(GenericHandler):
-
-    def get(self):
-        return self.render_template('signup.html')
-
-    def post(self):
-        username = str(self.request.get('username'))
-        password = str(self.request.get('password'))
-        verify = str(self.request.get('verify'))
-        email = str(self.request.get('email'))
-
-        username_error = False
-        if not ProblemSet2.validate_username(username):
-            username_error = 'Invalid username'
-
-        password_error = False
-        if not ProblemSet2.validate_password(password, verify=verify):
-            password_error = 'Invalid password'
-            password = verify = ''
-
-        email_error = False
-        if not ProblemSet2.validate_email(email):
-            email_error = 'Invalid E-mail'
-
-        return_context = {
-            'username': username,
-            'password': password,
-            'verify': verify,
-            'email': email,
-            'password_error': password_error,
-            'username_error': username_error,
-            'email_error': email_error,
-        }
-
-        return_context = {key: value for key, value in return_context.items() if value}
-
-        if not(username_error or password_error or email_error):
-            return self.redirect('/thanks?username=' + username)
+            return self.redirect('/')
         else:
-            return self.render_template('signup.html', return_context)
+            return_data = {
+                'error': 'We need both a title and art-work man! :(',
+                'art': art,
+                'title': title,
+            }
+            return self.render_page(context_data=return_data)
 
 
 page_list = [
     ('/', HomePageHandler),
-    ('/unit2/rot13', Rot13),
-    ('/unit2/signup', SignupHandler),
-    ('/thanks', ThanksHandler)
 ]
 app = webapp2.WSGIApplication(page_list, debug=True)

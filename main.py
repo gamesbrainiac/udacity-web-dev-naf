d1eba071282110ccbe0e70e60225c00ed4c7e366
Nafiul Islam <gamesbrainiac@gmail.com>
2013-06-22 21:52:39 +0600

    Problem set 3 done, first try, now solid core and text area works better. Now works with udemy course site.

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from google.appengine.ext import db
 # Setting directory for template files
 TEMPLATE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
 
-
+# Creating the jinja environment
 JINJA_ENV = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATE_DIR), autoescape=True)
 
 
-class Post(db.Model):
-    title = db.StringProperty(required=True)
+class PostModel(db.Model):
+    subject = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     created = db.DateTimeProperty(auto_now_add=True)
     updated = db.DateTimeProperty(auto_now=True)
@@ -34,18 +34,21 @@ class Handler(webapp2.RequestHandler):
         return self.write(template.render(**kwargs))
 
 
-# Where all the web-page views are
+########## Where all the web-page views are ####################
+
+# The homepage view
 class HomePage(Handler):
 
     def get(self):
         return self.render('HomePage.html')
 
 
+# view for our blog roll
 class Blog(Handler):
 
     def get(self):
         # Getting the top 10 most recent posts
-        list_of_posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")[:10]
+        list_of_posts = PostModel.all().order("-created")[:10]
 
         return_list = {
             'list_of_posts': list_of_posts,
@@ -54,40 +57,42 @@ class Blog(Handler):
         return self.render('Blog.html', **return_list)
 
 
+# View for creating a new post
 class NewPost(Handler):
 
     def get(self):
         return self.render('NewPost.html')
 
     def post(self):
-        title = str(self.request.get('title'))
+        subject = str(self.request.get('subject'))
         content = str(self.request.get('content'))
 
-        if title and content:
-            data = Post(title=title, content=content)
+        if subject and content:
+            data = PostModel(subject=subject, content=content)
             data.put()
 
             return self.redirect('/blog/%s' % data.key().id())
 
         error = ''
-        if not title:
-            error += 'There was no title.'
+        if not subject:
+            error += 'There was no subject.'
         if not content:
             error += ' There was no content.'
 
         return_list = {
             'error': error,
-            'title': title,
+            'subject': subject,
             'content': content,
         }
 
         return self.render('NewPost.html', **return_list)
 
 
+# View for going to a particular post
 class ParticularPost(Handler):
 
     def get(self, key_id):
-        key = db.Key.from_path('Post', int(key_id))
+        key = db.Key.from_path('PostModel', int(key_id))
         post = db.get(key)
 
         return_list = {
